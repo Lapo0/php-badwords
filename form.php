fb925348f772ecf494b3446182cbc440deb5fab4
Lapo0 <lapo.gini.01@gmail.com>
2023-03-17 16:57:58 +0100

    FEAT input textarea & input text

--- form.php
+++ form.php
@@ -1,3 +1,29 @@
-<?php 
+<?php
+
+    $paragrafo = $_GET['paragrafo'];
+    var_dump($_GET);
+
+    $parola = $_GET['parola'];
+    var_dump($_GET);
+
+    $paragrafo_censurato = str_replace($parola, '***', $paragrafo)
 
 ?>
+
+<h2> 
+    <?php echo $paragrafo ?> 
+</h2>
+
+<h2> Il tuo paragrafo è lungo 
+    <?php echo strlen($paragrafo) ?> 
+    caratteri
+</h2>
+
+<h4>
+    <?php echo $parola ?>
+</h4>
+
+<h1>
+    <?php echo $paragrafo_censurato ?>
+</h1>
+
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <form action="form.php" method="GET">
     <textarea name="paragrafo" id="" cols="30" rows="10">
-        
     </textarea>
+
+    <input type="text" name="parola">
+
+    <button type="submit"> INVIA </button>
 
 </form>
