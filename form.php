d5f3d563719807293c22e962fdebf7193e60945b
Lapo0 <lapo.gini.01@gmail.com>
2023-03-17 17:32:17 +0100

    REFACTOR $pargarfo_censurato

--- form.php
+++ form.php
@@ -9,24 +9,25 @@
     var_dump($_GET);
 
     // creare nuova variabile dove salvare il paragrafo con la parola selezionata censurata
-    $paragrafo_censurato = str_replace(strtoupper($parola), '***', strtoupper($paragrafo))
+    $paragrafo_censurato = str_replace(strtoupper($parola), '***', strtoupper($paragrafo));
+
+    // $paragrafo_minuscolo = strtolower($paragrafo_censurato);
 
 ?>
 
-
 <h2> 
     Il tuo paragrafo é : 
-    <?php echo ucfirst($paragrafo) ?>
+    <?php echo $paragrafo; ?>
     ed è lungo : 
-    <?php echo strlen($paragrafo) ?> 
+    <?php echo strlen($paragrafo); ?> 
     caratteri
 </h2>
 
 <h4>
-    <?php echo $parola ?>
+    <?php echo $parola; ?>
 </h4>
 
 <h1>
-    <?php echo $paragrafo_censurato ?>
+    <?php echo ucfirst(strtolower($paragrafo_censurato)); ?>
 </h1>
 
